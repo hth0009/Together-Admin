7010f64a56926c4b42764b2f66e1d9e256b36a7b
Hamilton Crews <dhamiltonc00@gmail.com>
2019-10-19 00:59:51 -0500

    me page NOT final. ready for early review. work needed on uploading profile picture and scrolling

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -5,7 +5,19 @@
         <div id="me-myself" class="panel gs-container vertical">
           <div class="header">
             <div class="profile-pic">
-              <avatar :height="100" :url="me.personImageThumbnailURL" :title="me.fullName" />
+              <div class="image-croppa">
+                <croppa v-model="photoCroppa"
+                  canvas-color="transparent"
+                  :disable-rotation="true"
+                  :prevent-white-space="true"
+                  :width="100"
+                  :height="100"
+                  :speed="10"
+                  v-show="editing"
+                  style="border-radius:100%;overflow:hidden"
+                ></croppa>
+              </div>
+              <avatar :height="100" :url="me.personImageThumbnailURL" :title="me.fullName" v-show="!editing" />
             </div>
             <h3>{{me.firstName + ' ' + me.lastName}}</h3>
             <div
@@ -76,7 +88,7 @@
                   :alphabetical="true"
                   :emptyMessage="'Not in any organizations'"
                   :cardList="myOrgs"
-                  :cardSelectable="true"
+                  :cardSelectable="false"
                   profilePicFillerValue="orgName"
                   :hasSearch="false"
                   :fields="{
@@ -96,7 +108,7 @@
                   :alphabetical="true"
                   :emptyMessage="'Not in any teams'"
                   :cardList="myTeams"
-                  :cardSelectable="true"
+                  :cardSelectable="false"
                   profilePicFillerValue="teamName"
                   :hasSearch="false"
                   :fields="{
@@ -116,12 +128,18 @@
 </template>
 
 <script>
-import Store from "../store";
-import People from "../services/people";
-import Church from "../services/church";
-import Teams from "../services/teams";
-import Avatar from "../components/Avatar";
+import Store from "../store"
+import People from "../services/people"
+import Church from "../services/church"
+import Teams from "../services/teams"
+import Avatar from "../components/Avatar"
 import Cards from '@/components/CardList'
+import Croppa from 'vue-croppa'
+import 'vue-croppa/dist/vue-croppa.css'
+import CDN from '@/services/cdn'
+import Vue from 'vue'
+Vue.use(Croppa)
+
 export default {
   name: "Me",
   data() {
@@ -161,14 +179,53 @@ export default {
         }))
       })
     },
+    async saveEdit() {      
+      this.editing = false 
+      var patch = {
+        "identifier":{
+          "id": Store.state.personID
+        },
+        "values": {
+          homeAddress: this.me.homeAddress,
+          mailingAddress: this.me.mailingAddress,
+          phoneNumber: this.me.phoneNumber
+        }
+      }
+      if (this.photoCroppa.hasImage()) {        
+        await CDN.getKeys().then(response => {
+          this.cdnKeys = response.data
+        })
+        var profilePic = await this.uploadProfilePic()
+        profilePic = !!profilePic ? 'https://togethercdn.global.ssl.fastly.net/PersonPics/' + profilePic : ''
+        patch['values']['personImageURL'] = profilePic
+        patch['values']['personImageThumbnailURL'] = profilePic
+      }
+      People.patchPerson(patch).then(() => {
+        this.getMe()
+      })
+    },
     startEdit() {
       this.editing = true
     },
     cancelEdit() {
       this.editing = false
-    },
-    saveEdit() {
-      this.editing = false
+    },    
+    async uploadProfilePic() {
+      const { accessKeyID, secretAccessKey } = this.cdnKeys
+      const fileSufix = 'PersonPics/'
+      var fileName = generateGUID() + '.jpg'
+      
+      if (!this.photoCroppa.hasImage()) {
+        return
+      }
+      var blob = await this.photoCroppa.promisedBlob('image/jpeg')
+      console.log(blob)
+      var arrayBuffer = await new Response(blob).arrayBuffer();  
+      await CDN.postImage(accessKeyID, secretAccessKey, arrayBuffer, fileSufix, fileName).catch(error => {
+        console.log(error)
+        fileName = ''
+      })
+      return fileName
     }
   },
   props: {},
