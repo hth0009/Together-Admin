ddcc3d7864b85864abd4a6d2e8e38f0994c77e91
Michael <mgh0010@auburn.edu>
2019-10-20 23:14:01 -0500

    added hint component

---
+++ src/components/Hint.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="hint-container">
+    {{input.length}} / {{maxNumOfCharsAllowed}}
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default {
+  name: "gsHint",
+  data() {
+    return {
+    }
+  },
+  props: [
+    'input',
+    'maxNumOfCharsAllowed'
+  ]
+}
+</script>
+
+<style>
+#hint-container {
+  text-align: right;
+  margin-top: 4px;
+}
+
+</style>
