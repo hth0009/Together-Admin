9b0e4e56ee3195b0a3824463849aa51635b757c3
mgh0010 <mghharrison@gmail.com>
2019-10-25 22:28:29 -0500

    make early access have animation and org name and go back to home screen afterwards

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="contact-container">    
-    <sweet-modal icon="success" ref="messageSent">
-      <h3>Message sent! We'll be in touch soon!</h3>
-    </sweet-modal>
     <form @submit.prevent="sendMail" >
       <input id="form-name" class="gs-basic-input" name="name" type="text" v-model="name" required placeholder="Name">
-      <input id="form-email" class="gs-basic-input" name="email" type="text" v-model="email" required placeholder="Email">
+      <input id="form-email" class="gs-basic-input" name="email" type="email" v-model="email" required placeholder="Email">
+      <input id="form-org-name" class="gs-basic-input" name="org-name" type="text" v-model="orgName" placeholder="Organization Name">
       <textarea id="form-message" class="gs-basic-input" name="message" v-model="message" required placeholder="Message"/>
+      <inline-loader v-show="loggingIn"></inline-loader>
       <button id="form-submit" class="gs-basic-button" type="submit">SUBMIT</button>
     </form>
   </div>    
@@ -15,43 +14,50 @@
 <script>
 import axios from 'axios'
 import { SweetModal } from 'sweet-modal-vue'
+import InlineLoader from '@/components/InlineLoader'
 
 export default {
   data() {
     return {
       message: '',
       email: '',
-      name: ''
+      orgName: '',
+      name: '',
+      loggingIn: false
     }
   },
   components: {
-    SweetModal
+    SweetModal,
+    InlineLoader
   },
   methods: {
     async sendMail(){
-      this.$root.$emit('loading', true)
-      axios.create().post('https://togetheradmin-contactus.prod.with-datafire.io/contact', {
-        name: this.name,
-        email: this.email,
-        message: this.message
-      }).then(response => {
-        this.$refs.messageSent.open()
+      this.loggingIn = true;
+      try {
+        await axios.create().post('https://togetheradmin-contactus.prod.with-datafire.io/contact', {
+          name: this.name,
+          email: this.email,
+          message: this.message
+        });
         this.$emit('successfullySent')        
         window.scrollTo(0,0);
         this.message = ''
         this.email = ''
+        this.orgName = ''
         this.name = ''
-      }).catch((error) => {
-        console.log(error)
-      }).then(response => {
-        this.$root.$emit('loading', false)
-      })
+      }
+      catch{(error) => {
+        throw error;
+      }}
+      finally {
+        this.loggingIn = false;
+      }
     }
   }
 }
 </script>
 
-<style>
+<style >
 #contact-container {
   width: 100%;
   max-width: 700px;
@@ -67,7 +73,7 @@ export default {
   width: 100%;
    display: grid;
    grid-template-columns: 1fr 1fr;
-   grid-template-rows: 1fr 5fr 1fr;
+   grid-template-rows: 1fr 1fr 5fr;
    grid-row-gap: 20px;
    grid-column-gap: 20px;
 }
@@ -76,21 +82,31 @@ export default {
 }
 #contact-container #form-name {
    grid-column: 1/2;
-   grid-row: 1 / 1;
+   grid-row: 1 / 2;
 }
 #contact-container #form-email {
    grid-column: 2/3;
-   grid-row: 1 / 1;
+   grid-row: 1 / 2;
+}
+#contact-container #form-org-name {
+   grid-column: 1 / 3;
+   grid-row: 2 / 3;
 }
 #contact-container #form-message {
    grid-column: 1 / 3;
-   grid-row: 2 / 2;
+   grid-row: 3 / 4;
+}
+.inline-loader-container {
+   grid-column: 1 / 3;
+   grid-row: 4 / 5;
+   justify-self: center;
 }
 #contact-container #form-submit { 
    grid-column: 1 / 3;
-   grid-row: 3 / 3;
+   grid-row: 5 / 6;
    justify-self: center;
 }
+
 @media all and (max-width: 800px) {
       #contact-container form {
       grid-template-columns: 1fr 1fr;
