e54ad1c543fdb10b9c1508dcdb383dbdef5f4ddc
Harrison, Michael <michael.harrison.contractor@bbva.com>
2019-12-17 12:20:58 -0600

    fix styling for manual entry giving

--- src/components/giving/GivingManualEntry.vue
+++ src/components/giving/GivingManualEntry.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="giving-manual-entry" class="giving-panel bold">
+  <div id="giving-manual-entry" class="giving-panel panel bold">
     <b-row>
       <b-col sm="8">
         <h4 class="black">Auburn Community Church</h4>
@@ -10,7 +10,8 @@
     </b-row>
     <b-row class="gs-flex-row">
       <b-col sm="6">
-        <button class="gs-floating-button">Amount</button>
+        <label for="name-amount-input">Amount</label>
+        <input type="number" name="giving-amount-input" class="gs-floating" placeholder="$50">
       </b-col>
       <b-col sm="6" class="right">
         <label for>Payment Date</label>
@@ -22,15 +23,15 @@
       </b-col>
     </b-row>
     <b-row>
-      <b-col sm="12" class="">
+      <b-col sm="12" class="mb_5em">
         Person
       </b-col>
-      <b-col sm="12">
-        <button class="gs-floating-button">Search</button>
+      <b-col sm="6">
+        <input type="number" name="giving-person-input" class="gs-floating" placeholder="search...">
       </b-col>
     </b-row>
     <b-row>
-      <b-col sm="12">
+      <b-col sm="12" class="mb_5em">
         Select Fund
       </b-col>
       <b-col sm="12" style="display: flex">
@@ -40,7 +41,7 @@
       </b-col>
     </b-row>
     <b-row>
-      <b-col sm="12">
+      <b-col sm="12" class="mb_5em">
         Type
       </b-col>
       <b-col sm="12" style="display: flex">
@@ -49,7 +50,7 @@
       </b-col>
     </b-row>
     <div>
-      <button class="gs-floating-button mr-5" style="float: right;">Create</button>
+      <button class="gs-floating-button primary mr-5" style="float: right;">Create</button>
     </div>
   </div>
 </template>
@@ -75,6 +76,6 @@ export default {
 
 <style scoped>
 .row {
-  margin-bottom: 1em !important;
+  margin-bottom: 2em !important;
 }
 </style>
--- src/components/giving/GivingTopNav.vue
+++ src/components/giving/GivingTopNav.vue
@@ -2,18 +2,18 @@
   <div id="giving-top-nav">
     <b-row class="mb2em">
       <b-col sm="3">
-        <input type="text" name="giving-search" class="floating" placeholder="search..." id="giving-search">
+        <input type="text" name="giving-search" class="gs-floating" placeholder="search..." id="giving-search">
       </b-col>
       <b-col sm="2">
-        <button name="giving-filter" id="giving-filter" class="gs-floating-button">Filter</button>
+        <button id="giving-filter" class="gs-floating-button">Filter</button>
       </b-col>
       <b-col sm="1" id="redo-icon">
         <font-awesome-icon :icon="['fas', 'redo']" />
       </b-col>
       <b-col sm="6" class="giving-creation-actions">
-        <button class="gs-floating-button" @click="startManualEntry()">Manual Entry</button>
-        <button class="gs-floating-button">Create Fund</button>
-        <button class="gs-floating-button">Create Report</button>
+        <button class="gs-floating-button" style="background-color: #D2D1CC" @click="startManualEntry()">Manual Entry</button>
+        <button class="gs-floating-button" style="background-color: #E5E9DD">Create Fund</button>
+        <button class="gs-floating-button" style="background-color: #F6F3F0">Create Report</button>
       </b-col>
     </b-row>
   </div>
@@ -36,13 +36,8 @@ export default {
 </script>
 
 <style scoped>
-  .floating {
-    padding: 10px 20px 10px 20px;
-    box-shadow: 0px 5px 13px -2px #00000040;
-    line-height: 1rem;
-    font-weight: 600;
-    border-radius: 1em;
-    width: 100%
+  #giving-filter {
+    color:#FC8888;
   }
   #redo-icon {
     display: flex;
